21d1e528529fde25c69b2176b9aed60ca0f78a4a
19691473550 <burakkutlukk@gmail.com>
2024-02-06 14:14:48 +0300

    todoapi

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
+import todoRoute from "./routes/todoRoute.js";
+
+dotenv.config();
+
+conn();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.static("public"));
+app.use(express.json({limit:'30mb', extended: "true"}));
+app.use(express.urlencoded({limit:'30mb', extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+//route
+
+app.use("/api", todoRoute)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
